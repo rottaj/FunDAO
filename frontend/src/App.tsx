f635b83d68bfe1ad2ecd8b313fc43397a1288348
rottaj <jackrotta12@gmail.com>
2021-12-19 22:04:11 -0500

    initialized frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.scss';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="FunDao-Header-Rainbow">
+                    <ul className="c-rainbow">
+                    <li className="c-rainbow__layer c-rainbow__layer--white">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--orange">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--red">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--violet">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--blue">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--green">FUN DAO!</li>
+                    <li className="c-rainbow__layer c-rainbow__layer--yellow">FUN DAO!</li>
+                </ul>
+                </div>
+      </div>
+    );
+  }
+
+}
+
+
