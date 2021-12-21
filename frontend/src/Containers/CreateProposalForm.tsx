7832ca87aad9a9e01c80923110fcd34a25c2ca17
rottaj <jackrotta12@gmail.com>
2021-12-20 22:54:49 -0500

    added proposal form.. will add series funding next

--- frontend/src/Containers/CreateProposalForm.tsx
+++ frontend/src/Containers/CreateProposalForm.tsx
@@ -10,7 +10,20 @@ export default class CreateProposalForm extends React.Component <Props>{
     render() {
         console.log(this.props.isOpen)
         return (
-            <div className="CreateProposalForm-Main">
+            <div className="CreateProposalForm-Main" >
+                {this.props.isOpen && 
+                <div className="PopUp-Form">
+                    <h3>Create New Proposal</h3>
+                    <form className="CreateProposal-Form">
+                        Requested Shares:
+                        <input type="text"/>
+                        <br></br>
+                        <br></br>
+                        Vested Shares:
+                        <input type="text"/>
+                    </form>
+                </div>
+                }
             </div>
         )
     }
