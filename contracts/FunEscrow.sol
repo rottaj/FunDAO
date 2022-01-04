9546b4fc795893448784902745fcb52194fc506a
rottaj <jackrotta12@gmail.com>
2022-01-04 15:48:19 -0500

    compiled.. ready for tests

--- contracts/FunEscrow.sol
+++ contracts/FunEscrow.sol
@@ -4,21 +4,21 @@ import "@openzeppelin/contracts/utils/escrow/RefundEscrow.sol";
 import "@openzeppelin/contracts/utils/escrow/ConditionalEscrow.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FunEscrow is Ownable, RefundEscrow{
+abstract contract FunEscrow is Ownable, RefundEscrow{
   address private _beneficiary;
-  constructor(address beneficiary_) public {
+  constructor(address beneficiary_) {
     _beneficiary = beneficiary_; 
   }
   
   function depositEther() public payable {
     deposit(msg.sender);
-    Deposited(msg.sender, msg.value);
+    emit Deposited(msg.sender, msg.value);
   }
 
   function withdrawEther(address payable _payee) public payable {
     require(withdrawalAllowed(_payee), "NOT ALLOWED TO WITHDRAWAL");
     withdraw(_payee);
-    Withdrawn(_payee, msg.value);
+    emit Withdrawn(_payee, msg.value);
   }
   
   function isAllowedForWithdrawal(address _payee) public view returns (bool){
